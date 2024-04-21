eeffc4aa3241a0d157769b3417951458a240c2ca
Faizantechy <faizanraza112112@gmail.com>
2024-04-21 16:10:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+var body=document.querySelector('body');
+
+function changeColor(name){
+    body.style.backgroundColor=name;
+}
+
+
+
+function rColor(){
+
+    var r = Math.round(Math.random()*255);
+    var g = Math.round(Math.random()*255);
+    var b = Math.round(Math.random()*255);
+    body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+
+}
+
+rColor();
